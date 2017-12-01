8fa5d5f1707f76900ec03b8dac6e5ec940b23efd
cesar <cesar.ferreira.tn@gmail.com>
2017-12-01 11:53:13 -0300

    criando projeto

---
+++ app/src/main/java/com/example/cesar/conversormoedas/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.cesar.conversormoedas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewHolder mViewHolder = new ViewHolder();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.mViewHolder.editValue = findViewById(R.id.edit_value);
+        this.mViewHolder.textDollar = findViewById(R.id.text_dollar);
+        this.mViewHolder.textEuro = findViewById(R.id.text_euro);
+        this.mViewHolder.buttonCalculate = findViewById(R.id.button_calculate);
+    }
+
+    private static class ViewHolder{
+
+
+        EditText editValue;
+        TextView textDollar;
+        TextView textEuro;
+        Button buttonCalculate;
+
+    }
+}
