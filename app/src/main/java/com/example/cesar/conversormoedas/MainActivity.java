a776c1ea6cea58f5ffd0863c3f30a8efa37c95c9
cesar <cesar.ferreira.tn@gmail.com>
2017-12-01 14:03:04 -0300

    lógica de botão

--- app/src/main/java/com/example/cesar/conversormoedas/MainActivity.java
+++ app/src/main/java/com/example/cesar/conversormoedas/MainActivity.java
@@ -2,11 +2,12 @@ package com.example.cesar.conversormoedas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private ViewHolder mViewHolder = new ViewHolder();
 
@@ -19,6 +20,28 @@ public class MainActivity extends AppCompatActivity {
         this.mViewHolder.textDollar = findViewById(R.id.text_dollar);
         this.mViewHolder.textEuro = findViewById(R.id.text_euro);
         this.mViewHolder.buttonCalculate = findViewById(R.id.button_calculate);
+
+        this.mViewHolder.buttonCalculate.setOnClickListener(this);
+
+        this.clearValues();
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        int id = view.getId();
+        if(id == R.id.button_calculate){
+
+            Double value = Double.valueOf(this.mViewHolder.editValue.getText().toString());
+
+            this.mViewHolder.textDollar.setText(String.format("%.2f", value * 3.2706));
+            this.mViewHolder.textEuro.setText(String.format("%.2f", value * 3.884));
+        }
+    }
+
+    private void clearValues(){
+        this.mViewHolder.textDollar.setText("");
+        this.mViewHolder.textEuro.setText("");
     }
 
     private static class ViewHolder{
